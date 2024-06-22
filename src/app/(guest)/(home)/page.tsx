4a64e5860260bfebcf92a8fa5e44bba6c3095293
Alo Timothy <alotimothy01@gmail.com>
2024-06-22 02:15:28 +0100

    Updates to landing

--- src/app/(guest)/(home)/page.tsx
+++ src/app/(guest)/(home)/page.tsx
@@ -3,7 +3,9 @@ import CategoriesSection from "@/components/home/CategoriesSection";
 import TeamSection from "@/components/home/TeamSection";
 import ImpactSection from "@/components/home/ImpactSection";
 import PartnershipSection from "@/components/home/PartnershipSection";
-
+import Testimonial15 from "@/components/home/Testimonial15";
+import Stats from "@/components/home/Stats";
+import Footer from "@/components/shared/Footer";
 export default function Home() {
   return (
     <main className="w-full min-h-screen">
@@ -12,6 +14,9 @@ export default function Home() {
       <TeamSection />
       <ImpactSection />
       <PartnershipSection />
+        <Testimonial15 />
+        <Stats />
+
     </main>
   );
 }
